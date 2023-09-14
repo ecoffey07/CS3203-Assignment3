c638ce6ce4b5a5715ad0220d74a1bea659b8abbc
Ben <bayokell@gmail.com>
2023-09-13 22:38:27 -0500

    Added maximum cout

--- app.cpp
+++ app.cpp
@@ -33,5 +33,6 @@ int main(){
     numbers = {1, 2, 3, 4, 5};
     cout << "Total: " << add(numbers) << endl;
     cout << "Product: " << multiply(numbers);
+    cout << "Maximum: " << maximum(numbers);
     return 0;
 }

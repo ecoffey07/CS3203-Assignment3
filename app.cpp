b65eb264617d7009039b24ed156ba3f775c2c15f
Ben <bayokell@gmail.com>
2023-09-13 22:12:49 -0500

    Introduce Main method, and have it call the add method

--- app.cpp
+++ app.cpp
@@ -11,3 +11,9 @@ int add(vector<int> a){
     return total;
 }
 
+int main(){
+    vector<int> numbers;
+    numbers = {1, 2, 3, 4, 5};
+    cout << "Total: " << add(numbers) << endl;
+    return 0;
+}
